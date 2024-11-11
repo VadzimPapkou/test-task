8d54e95c45637f7c2ebd05c1d749fa7c809874aa
VadzimPapkou <playvarian1@gmail.com>
2024-11-11 20:04:01 +0300

    fix eslint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import styles from "./page.module.scss";
-import {Step} from "@/components/Step/Step";
+import Image from "next/image";
 import {Button} from "@/components/Button/Button";
 import {Steps} from "@/components/Steps/Steps";
 
@@ -8,7 +8,7 @@ export default function Home() {
         <main className={styles.main}>
             <Steps className={styles.steps}/>
             <div className={styles.buttonContainer}>
-                <Button className={styles.button}><img src="/shoppingCart.svg" alt=""/> Стать партнером</Button>
+                <Button className={styles.button}><Image src="/shoppingCart.svg" alt=""/> Стать партнером</Button>
             </div>
         </main>
     );
--- src/components/Steps/Steps.tsx
+++ src/components/Steps/Steps.tsx
@@ -1,8 +1,8 @@
 import styles from "./Steps.module.scss";
 
 import {Step} from "@/components/Step/Step";
-import {ReactNode} from "react";
 import clsx from "clsx";
+import Image from "next/image";
 
 
 interface StepsProps {
@@ -16,16 +16,16 @@ export const Steps = ({className}: StepsProps) => {
             <div className={styles.stepsList}>
                 <Step className={styles.stepAffiliation} stepCount={1}
                       title={<span>Получите <br/>партнерскую ссылку</span>}>
-                    <img className={styles.stepImage} src="/laptopMan.png" alt="man on laptop"/>
-                    <img className={styles.arrow} src="/arrow.png" alt=""/>
+                    <Image className={styles.stepImage} src="/laptopMan.png" alt="man on laptop"/>
+                    <Image className={styles.arrow} src="/arrow.png" alt=""/>
                 </Step>
                 <Step className={styles.stepAbout} stepCount={2} title={<span>Расскажите <br/>о iiii.COM</span>}>
-                    <img className={styles.stepImage} src="/peopleLookPhones.png" alt="people look at phones"/>
-                    <img className={styles.arrow} src="/arrow.png" alt=""/>
+                    <Image className={styles.stepImage} src="/peopleLookPhones.png" alt="people look at phones"/>
+                    <Image className={styles.arrow} src="/arrow.png" alt=""/>
                 </Step>
                 <Step className={styles.stepPercent} stepCount={3}
                       title={<span>Получайте хороший процент <br/>от покупок по вашей ссылке!</span>}>
-                    <img className={styles.stepImage} src="/manEarning.png" alt="man with money"/>
+                    <Image className={styles.stepImage} src="/manEarning.png" alt="man with money"/>
                 </Step>
             </div>
         </div>
